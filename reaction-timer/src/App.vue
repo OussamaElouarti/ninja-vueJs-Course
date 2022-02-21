0a21649fb1616b7b02364f3bfd424eb5005c51f4
El Ouarti <o.elouarti03@gmail.com>
2022-02-21 18:30:29 +0100

    reaction-timer

---
+++ reaction-timer/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <h1>Tesla reaction timer</h1>
+  <button @click="start" :disabled="isPlaying">play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Results v-if="showScore" :score="score" />
+</template>
+
+<script>
+
+import Block from "./components/Block.vue"
+import Results from "./components/Results.vue"
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showScore: false
+    }
+  },
+  components: { Block, Results },
+  methods: {
+    start() {
+        this.delay = 2000 + Math.random() * 5000
+        this.isPlaying = true
+        this.showScore = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showScore = true
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
---
+++ reaction-timer/src/components/Results.vue
@@ -0,0 +1,50 @@
+<template>
+  <p>reaction time: {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data () {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 100)
+            this.rank = "Nadi..."
+        else if (this.score < 250)
+            this.rank = "mabikch..."
+        else if (this.score < 400) {
+            this.rank = 'naqs...'
+        }
+        else {
+            this.rank = '7alazona...'
+        }
+    }
+}
+</script>
+
+<style>
+    .rank {
+        font-size: 1.4em;
+        color: #0faf87;
+        font-weight: bold;
+    }
+    button {
+        background-color: #0faf87;
+        color: white;
+        border: none;
+        padding: 8px 16px;
+        border-radius: 4px;
+        font-size: 16px;
+        cursor: pointer;
+        letter-spacing: 1px;
+        margin: 10px;
+    }
+    button[disabled] {
+        opacity: 0.2;
+        cursor: not-allowed;
+    }
+</style>
